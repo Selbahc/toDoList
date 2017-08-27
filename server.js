ff224dfa48ff4d6193aa6ec5656135ae820d3ecb
Selbahc <selbahc@users.noreply.github.comm>
2017-08-27 17:23:58 +0200

    added Sass and some styling

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const routes = require('./routes');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose'); //mongoose to access mongodb database
+const sass = require('node-sass-middleware');
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', path.resolve(__dirname, 'views'));
 
+app.use(sass({
+  src: path.resolve(__dirname, 'sass'), //css & scss : same name !!
+  dest: path.resolve(__dirname, 'public', 'stylesheets'),
+  prefix: '/stylesheets'
+}));
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(routes);
 
 mongoose.connect('mongodb://localhost/todolist', {
